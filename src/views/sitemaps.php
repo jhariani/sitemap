8f0794f5ed8f625e5847ecfdc7952f548a4d0bf3
Jay Hariani <jhariani@gmail.com>
2015-01-23 12:00:41 -0500

    Allow sitemap indexes to have a 'lastmod' property

--- src/views/sitemaps.php
+++ src/views/sitemaps.php
@@ -3,6 +3,9 @@
   <?php foreach ($sitemaps as $sitemap): ?>
   <sitemap>
     <loc><?= $sitemap['loc'] ?></loc>
+    <?php if ($sitemap['lastmod']): ?>
+        <lastmod><?= date('Y-m-d\TH:i:sP', strtotime($sitemap['lastmod'])) ?></lastmod>
+    <?php endif ?>
   </sitemap>
   <?php endforeach ?>
 </sitemapindex>
